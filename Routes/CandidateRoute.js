fdf1ed5cb922157ba933675f7a3127a051752902
Harsh202002 <harshit@netfotech.in>
2025-09-10 14:30:34 +0530

    some changes made in the backend apis

--- Routes/CandidateRoute.js
+++ Routes/CandidateRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import upload from '../Middlewares/upload.js';
-import { loginCandidate, registerCandidate, applyToSpecificJD,addCandidateDetails,getCandidateProfile,getAllCandidatesdataAccordingToJD,getAllAppliedJobs } from '../Controllers/CandidateController.js';
+import { loginCandidate, registerCandidate, applyToSpecificJD,addCandidateDetails,getCandidateProfile,getAllCandidatesdataAccordingToJD,getAllAppliedJobs,updateCandidateAdditionalDetails,updateCandidateBasicDetails,getCandidateAdditionalDetails } from '../Controllers/CandidateController.js';
 import protect from '../Middlewares/authMiddleware.js';
 import { authorize } from '../Middlewares/authorize.js';
 
@@ -11,7 +11,10 @@ router.post("/register-candidate", registerCandidate)
 router.post("/login-candidate", loginCandidate)
 router.post('/jobs/:jobId/apply', protect, authorize('candidate'), upload.single('resume'), applyToSpecificJD);
 router.post('/add-additionalDetails',addCandidateDetails);
-router.get('/getcandidate-profile/:candidateId',getCandidateProfile)
+router.get('/getcandidate-profile',protect, authorize('candidate'),getCandidateProfile)
 router.get('/get-all-candidates/:jobId', getAllCandidatesdataAccordingToJD);
 router.get('/get-all-applied-jobs',protect,authorize('candidate'),getAllAppliedJobs);
+router.put('/update-candidate-additional-details/:candidateId',updateCandidateAdditionalDetails)
+router.put('/update-candidate-basic-details/:candidateId',updateCandidateBasicDetails)
+router.get('/get-candidate-additional-details/:candidateId',getCandidateAdditionalDetails)
 export default router;
